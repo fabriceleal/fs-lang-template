15e89429a64b9009d7938f72141dd26494df26fe
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-06-01 14:15:08 +0100

    first commit - working

---
+++ LangTemplate/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+module LangTemplate
+
+open Microsoft.FSharp.Text.Lexing
+
+[<EntryPoint>]
+let main argv = 
+    let tree = Parser.Start Lexer.tokenstream (LexBuffer<char>.FromString "...")
+    printfn "%A" tree
+    0 // return an integer exit code
